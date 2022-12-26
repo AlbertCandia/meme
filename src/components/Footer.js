10abdfda3d2f01dc49c6636caae8df4de1aa3200
AlbertCandia <kity_lilo@yahoo.com.ar>
2022-12-26 19:20:56 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../stylesheets/footer.css';
+const Footer = () => {
+  return (
+    <footer>
+      Memes By{' '}
+      <a
+        href="https://github.com/AlbertCandia"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Alberto
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="teal">
+      <p className="brand-logo">Generador de Memes CaC.</p>
+    </nav>
+  );
+};
+
+export default Navbar;
